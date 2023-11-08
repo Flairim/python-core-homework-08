fa385451a88c900be01b190c184df3dcb2792a6d
Олексій Тригуб <149113900+Flairim@users.noreply.github.com>
2023-11-08 20:38:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-from datetime import date, datetime
+from datetime import date, timedelta, datetime
+from collections import defaultdict
 
 
-def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+start_date = date.today()
+end_date = start_date + timedelta(7)
 
+def get_period(start_date: date, days: int): 
+                                            
+    result = {}
+    for _ in range(days + 1):
+        result[start_date.day, start_date.month] = start_date.year
+        start_date += timedelta(1)
+    
+    return result
+
+def get_birthdays_per_week(users: list) -> list:
+
+    res = defaultdict(list)
+
+    start_date = date.today()
+    period = get_period(start_date, 7)
+    
+
+    if not users:
+        res = {}
+        return res
+         
+    for user in users:
+        bd: date = user["birthday"]
+        date_bd = bd.day, bd.month
+        
+        if date_bd in list(period):
+            
+            date_bd_week = bd.replace(year=period[date_bd])
+            
+            bd__weekday = date_bd_week.weekday()
+           
+            if bd__weekday in (5, 6):
+                res["Monday"].append(user["name"])
+            else:
+                res[date_bd_week.strftime("%A")].append(user["name"])
+          
+    return res
 
 if __name__ == "__main__":
     users = [
